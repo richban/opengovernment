ab6e9dc6f2f96767393a57fbeb296816378728a7
richban <richardbanyi@Richards-MacBook-Pro.local>
2016-04-09 00:17:38 +0200

    corrected loop iteration

--- postgres/clean_data.py
+++ postgres/clean_data.py
@@ -4,7 +4,7 @@ path_sources = '/Users/richardbanyi/Developer/Environments/Data/dw/Data_Sources/
 path_cleaned = '/Users/richardbanyi/Developer/Environments/Data/dw/cleaned_data/'
 csv_files = glob.glob(path_sources)
 
-for file in len(csv_files):
+for file in csv_files:
     source = open(file, 'r')
     source_name = os.path.basename(file).split('_')
     cleaned_csv = open(path_cleaned+source_name[0]+'_cleaned.csv', 'w')
