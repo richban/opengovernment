8b3f0c4667250529059e2c0e81f3a5216373ecd8
richban <richardbanyi@Richards-MacBook-Pro.local>
2016-05-03 01:45:10 +0200

    data cleaning & transformation updates

--- postgres/data_quality-geography.sql
+++ postgres/data_quality-geography.sql
@@ -1,16 +1,6 @@
 -- replace values for foreign entities
 
--- Correct States done during load
-
-select distinct
-	case
-		when a.principal_place_country_code = b.country_code then b.country_name
-		else a.principal_place_country_code
-	end as country_name
-from base_fin a join country_codes_names b
-on a.principal_place_country_code = b.country_code;
-
--- Update #0
+-- Update #0 Correct Coutry full names
 
 create table country_codes_names(
 	country_code text,
@@ -23,101 +13,115 @@ select distinct
 	split_part(placeofperformancecountrycode, ': ', 2) as country_name
 from base_contracts;
 
+select distinct
+	case
+		when a.principal_place_country_code = b.country_code then b.country_name
+		else a.principal_place_country_code
+	end as country_name
+from base_fin a join country_codes_names b
+on a.principal_place_country_code = b.country_code;
+
+
 -- Update #1 Geography
 
-update load_data_2 set
-	geography_country = 'usa',
-  recipient_country = 'usa'
+update load_data set
+	geography_country = 'united states of america',
+  	recipient_country = 'united states of america'
 	where
-    geography_country = 'united states of america' or
-    recipient_country = 'united states of america';
+    geography_country = 'usa' or
+    recipient_country = 'usa';
 
 -- Update #2
 
-update load_data_2 set
+update load_data set
 	geography_state = 'forgn',
 	geography_city = 'forgn',
 	geography_zip = 'forgn'
 where geography_country != 'united states of america' or geography_country = 'n\a';
 
--- Update #3
+-- Update #3 NOT USED! NEW REPLACEMENT
 
 update load_data_2 set
 	geography_city = 'n\a',
   geography_state = 'n\a',
-  recipient_state = 'n\a'
+  recipient_state = 'n\a',
+  recipient_city = 'n\a'
 where
   geography_city ~ '(\d+)' or
   geography_state ~ '(\d+)' or
-  recipient_state '(\d+)';
+  recipient_state ~ '(\d+)' or
+  recipient_city ~ '(\d+)';
 
 
 -- Update #4
 
-update load_data_2
+update load_data
 set geography_state =
 	case geography_state
   	when 'fl' then 'florida'
-	when 'la' then 'louisiana'
-	when 'or' then 'oregon'
-	when 'ms' then 'mississippi'
-	when 'il' then 'illinois'
-	when 'in' then 'indiana'
-	when 'ut' then 'utah'
-	when 'mi' then 'michigan'
-	when 'ks' then 'kansas'
-	when 'mn' then 'minnesota'
-	when 'ne' then 'nebraska'
-	when 'ct' then 'connecticut'
-	when 'al' then 'alabama'
-	when 'ca' then 'california'
-	when 'nd' then 'north dakota'
-	when 'wa' then 'washington'
-	when 'ky' then 'kentucky'
-	when 'ri' then 'rhode island'
-	when 'va' then 'virginia'
-	when 'ia' then 'iowa'
-	when 'ny' then 'new york'
-	when 'ma' then 'massachusetts'
-	when 'tx' then 'texas'
-	when 'pa' then 'pennsylvania'
-	when 'az' then 'arizona'
-	when 'mt' then 'montana'
-	when 'nm' then 'new mexico'
-	when 'ak' then 'alaska'
-	when 'nc' then 'north carolina'
-	when 'vt' then 'vermont'
-	when 'ar' then 'arkansas'
-	when 'mo' then 'missouri'
-	when 'wy' then 'wyoming'
-	when 'md' then 'maryland'
-	when 'vi' then 'virginia'
-	when 'ga' then 'georgia'
-	when 'wi' then 'wisconsin'
-	when 'dc' then 'district of columbia'
-	when 'oh' then 'ohio'
-	when 'nv' then 'nevada'
-	when 'ok' then 'oklahoma'
-	when 'co' then 'colorado'
-	when 'wv' then 'west virginia'
-	when 'de' then 'delaware'
-	when 'me' then 'maine'
-	when 'tn' then 'tennessee'
-	when 'sd' then 'south dakota	'
-	when 'nh' then 'new hampshire'
-	when 'sc' then 'south carolina'
-	when 'id' then 'idaho'
-	when 'nj' then 'new rersey'
-	when 'mp' then 'northern mariana islands'
-	when 'pr' then 'puerto rico'
-	when 'gu' then 'guam'
-	when 'hi' then 'howland island'
-	when 'as' then 'american samoa'
-  when 'mh' then 'marshall islands'
+    when 'la' then 'louisiana'
+    when 'or' then 'oregon'
+    when 'ms' then 'mississippi'
+    when 'il' then 'illinois'
+    when 'in' then 'indiana'
+    when 'ut' then 'utah'
+    when 'mi' then 'michigan'
+    when 'ks' then 'kansas'
+    when 'mn' then 'minnesota'
+    when 'ne' then 'nebraska'
+    when 'ct' then 'connecticut'
+    when 'al' then 'alabama'
+    when 'ca' then 'california'
+    when 'nd' then 'north dakota'
+    when 'wa' then 'washington'
+    when 'ky' then 'kentucky'
+    when 'ri' then 'rhode island'
+    when 'va' then 'virginia'
+    when 'ia' then 'iowa'
+    when 'ny' then 'new york'
+    when 'ma' then 'massachusetts'
+    when 'tx' then 'texas'
+    when 'pa' then 'pennsylvania'
+    when 'az' then 'arizona'
+    when 'mt' then 'montana'
+    when 'nm' then 'new mexico'
+    when 'ak' then 'alaska'
+    when 'nc' then 'north carolina'
+    when 'vt' then 'vermont'
+    when 'ar' then 'arkansas'
+    when 'mo' then 'missouri'
+    when 'wy' then 'wyoming'
+    when 'md' then 'maryland'
+    when 'vi' then 'virginia'
+    when 'ga' then 'georgia'
+    when 'wi' then 'wisconsin'
+    when 'dc' then 'district of columbia'
+    when 'oh' then 'ohio'
+    when 'nv' then 'nevada'
+    when 'ok' then 'oklahoma'
+    when 'co' then 'colorado'
+    when 'wv' then 'west virginia'
+    when 'de' then 'delaware'
+    when 'me' then 'maine'
+    when 'tn' then 'tennessee'
+    when 'sd' then 'south dakota	'
+    when 'nh' then 'new hampshire'
+    when 'sc' then 'south carolina'
+    when 'id' then 'idaho'
+    when 'nj' then 'new rersey'
+    when 'mp' then 'northern mariana islands'
+    when 'pr' then 'puerto rico'
+    when 'gu' then 'guam'
+    when 'hi' then 'howland island'
+    when 'as' then 'american samoa'
+    when 'mh' then 'marshall islands'
+    when 'd.c' then 'district of columbia'
+    when 'd.c.' then 'district of columbia'
+    when 'dc' then 'district of columbia'
 	else
 		lower(geography_state)
 	end
-where length(geography_state) <3;
+where length(geography_state) < 5;
 
 
 --
@@ -185,26 +189,50 @@ set recipient_state =
 	when 'as' then 'american samoa'
   when 'mh' then 'marshall islands'
   when 'd.c' then 'district of columbia'
+  when 'd.c.' then 'district of columbia'
+  when 'dc' then 'district of columbia'
 	else
 		lower(recipient_state)
 	end
-where length(recipient_state) <3;
+where length(recipient_state) < 3;
 
 --
--- Update #6
+-- Update #3
 --
 
 update load_data set
-	recipient_state =
-  regexp_replace(recipient_state, '([\-\+\.\^:,*])+', '');
+	recipient_state = coalesce(nullif(trim(both '   ' from regexp_replace(recipient_state, '([\-\+\.\"\#\^:,*''\d])+', '', 'g')), ''), 'n\a'),
+  recipient_city = coalesce(nullif(trim(both '   ' from regexp_replace(recipient_city, '([\-\+\.\"\#\^:,*''\d])+', '', 'g')), ''), 'n\a'),
+  geography_city = coalesce(nullif(trim(both '   ' from regexp_replace(geography_city, '([\-\+\.\"\#\^:,*''\d])+', '', 'g')), ''), 'n\a'),
+  geography_state = coalesce(nullif(trim(both '   ' from regexp_replace(geography_state, '([\-\+\.\"\#\^:,*''\d])+', '', 'g')), ''), 'n\a');
 
+
+--
+-- Update #3.1 Correct zipcodes
+--
+
+update load_data set
+	recipient_zip =
+	case
+		when length(trim(both ' 	' from regexp_replace(recipient_zip, '(\D)', '', 'g'))) < 5 then 'n\a'
+		else trim(both ' 	' from regexp_replace(recipient_zip, '(\D)', '', 'g'))
+	end,
+	geography_zip =
+	case
+		when length(trim(both ' 	' from regexp_replace(geography_zip, '(\D)', '', 'g'))) < 5 then 'n\a'
+		else trim(both ' 	' from regexp_replace(geography_zip, '(\D)', '', 'g'))
+	end;
 
 --
 -- Update #7
 --
 
 update load_data set
-	recipient_city = coalesce(nullif(trim(regexp_replace(recipient_city, '[^a-zA-Z \\]', '')), ''), 'n\a');
+	recipient_city = coalesce(nullif(trim(both '  ' from regexp_replace(recipient_city, '[^a-zA-Z \\]', '')), ''), 'n\a');
+
+update load_data set
+	recipient_city = coalesce(nullif(trim(both '   ' from regexp_replace(recipient_city, '/([\-\+\.\"\^:,*''])+/g', '')), ''), 'n\a');
+
 
 --
 -- Update #8
@@ -250,4 +278,6 @@ geography_zip =
 	case
 		when geography_zip = 'n\a' and recipient_zip != 'n\a' then recipient_zip
 		else geography_zip
-	end;
+	end
+where geography_country = 'united states of america' and
+(recipient_country = 'united states of america' or recipient_country = 'n\a');
